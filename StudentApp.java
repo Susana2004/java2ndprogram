ed2a4014f65eb5a5711f4cf2cecf5240adea54f8
Susana2004 <gs6986164@gmail.com>
2025-05-14 14:42:43 +0530

    added new line

--- StudentApp.java
+++ StudentApp.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 class Student {
     private String name;
@@ -23,9 +24,34 @@ class Student {
     }
 }
 
+class StudentCourses {
+    private Map<String, Map<String, Integer>> semesterCourses = new HashMap<>();
+
+    public void addCourse(String semester, String course, int marks) {
+        semesterCourses.putIfAbsent(semester, new HashMap<>());
+        semesterCourses.get(semester).put(course, marks);
+    }
+
+    public void displayCourses() {
+        for (String semester : semesterCourses.keySet()) {
+            System.out.println("Semester: " + semester);
+            Map<String, Integer> courses = semesterCourses.get(semester);
+            for (String course : courses.keySet()) {
+                System.out.println("Course: " + course + ", Marks: " + courses.get(course));
+            }
+        }
+    }
+}
+
 public class StudentApp {
     public static void main(String[] args) {
         Student student = new Student("Alice", "15-08-2003");
         student.displayInfo();
+
+        StudentCourses sc = new StudentCourses();
+        sc.addCourse("Semester 1", "Math", 88);
+        sc.addCourse("Semester 1", "Physics", 75);
+        sc.addCourse("Semester 2", "Chemistry", 90);
+        sc.displayCourses();
     }
 }
